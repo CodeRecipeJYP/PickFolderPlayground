006f9fba3f6423bdaab189e419d7b90f9caf136d
kujyp <pjo901018@gmail.com>
2018-04-16 14:45:48 +0900

    Make it Checkable

--- app/src/main/java/com/asuscomm/yangyinetwork/pickfolderplayground/FolderViewHolder.kt
+++ app/src/main/java/com/asuscomm/yangyinetwork/pickfolderplayground/FolderViewHolder.kt
@@ -2,12 +2,20 @@ package com.asuscomm.yangyinetwork.pickfolderplayground
 
 import android.support.v4.graphics.drawable.DrawableCompat
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.item_folder.view.*
 
 class FolderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    companion object {
+        const val TAG = "FolderViewHolder"
+    }
     init {
         itemView.iv_folder.setImageDrawable(itemView.iv_folder.drawable.mutate())
+        itemView.setOnClickListener {
+            Log.d(TAG, "clicked, itemView.iv_folder.isSelected = ${itemView.iv_folder.isSelected}")
+            itemView.iv_folder.isSelected = !itemView.iv_folder.isSelected
+        }
     }
 
     fun configure(folder: Folder) {
